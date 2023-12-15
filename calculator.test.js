2b30f1d405b1069db1071b7f5917ff3d06dc8b5e
josetj101 <josetj101@gmail.com>
2023-12-16 00:05:56 +0530

    test: Validate Add function to input only string

--- calculator.test.js
+++ calculator.test.js
@@ -3,8 +3,11 @@ const Add = require('./calculator');
 describe('String Calculator', () => {
   test('Validate Add function', () => {
     expect(Add).toBeDefined();
-  })
+  });
   test('Validate Add is a function', () => {
     expect(Add).toBeFunction();
+  });
+  test('Validate Add function to input only string', () => {
+    expect(() => Add(0)).toThrowWithMessage(TypeError, 'Invalid input');
   })
 });
